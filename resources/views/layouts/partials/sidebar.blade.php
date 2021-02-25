0a1559977356c35da8bfad5909b730bfa16f79e5
Francesco <fraancesco.bello@gmail.com>
2021-02-25 15:12:17 +0100

    visualizzazione p in sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -3,39 +3,39 @@
 
 
 <div class="sidebar-top">
-    <div class="sidebar-link">
-        <ul>
-            <li><a href="{{ route('home') }}"><i class="fas fa-home"></i><p class="text">Homepage</p> </a> </li>
-            <li><a href="{{ route('contacts') }}"><i class="far fa-envelope"></i><p class="text">Contact Us</p> </a> </li>
+  <div class="sidebar-link">
+    <ul>
+        <li><a href="{{ route('home') }}"><i class="fas fa-home"></i><p class="text">Homepage</p> </a> </li>
+        <li><a href="{{ route('contacts') }}"><i class="far fa-envelope"></i><p class="text">Contact Us</p> </a> </li>
 
-            @if (Route::has('login'))
+        @if (Route::has('login'))
 
-                @auth
-                    <li>
-                        <a href="{{ route('login') }}"><i class="fas fa-qrcode"></i><p class="text">Dashboard</p></a> 
-                    </li>
-                    <li>
-                    {{-- aggiunto logout frontoffice --}}
-                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            <i class="fas fa-sign-out-alt"></i><p class="text">LogOut</p>
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </li>
+            @auth
+                <li>
+                    <a href="{{ route('login') }}"><i class="fas fa-qrcode"></i><p class="text">Dashboard</p></a>
+                </li>
+                <li>
+                {{-- aggiunto logout frontoffice --}}
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fas fa-sign-out-alt"></i><p class="text">LogOut</p>
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
 
-                @else
-                    <li>
-                        <a href="{{ route('login') }}"><i class="fas fa-sign-in-alt"></i><p class="text">LogIn</p> </a> 
-                    </li>
+            @else
+                <li>
+                    <a href="{{ route('login') }}"><i class="fas fa-sign-in-alt"></i><p class="text">LogIn</p> </a>
+                </li>
 
-                    @if (Route::has('register'))
-                        <li><a href="{{ route('register') }}">Register </a> </li>
-                    @endif
+                @if (Route::has('register'))
+                    <li><a href="{{ route('register') }}">Register </a> </li>
+                @endif
 
-                @endauth
+            @endauth
 
-            @endif
-        </ul>
-    </div>
+        @endif
+    </ul>
+  </div>
 </div>
