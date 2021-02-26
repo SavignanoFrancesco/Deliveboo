d91e0ee64439f46bbf33cf7a07e495252285fa44
Alberto <thebertsinfo@gmail.com>
2021-02-26 15:51:45 +0100

    Redirect create restaurant after register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::RESTAURANT;
 
     /**
      * Create a new controller instance.
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,6 +22,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/admin';
+    public const RESTAURANT = '/admin/restaurant/create';
 
     /**
      * Define your route model bindings, pattern filters, etc.
